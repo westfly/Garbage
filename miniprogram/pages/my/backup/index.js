aacd58e407f7cf672834a020428826ee54fbec81
Rayan <yweiyun@gmail.com>
2019-08-04 20:07:05 +0800

    port my from EastWorld/wechat-app-mall

---
+++ miniprogram/pages/my/backup/index.js
@@ -0,0 +1,91 @@
+const app = getApp()
+const CONFIG = require('../../config.js')
+const WXAPI = require('../../wxapi/main')
+Page({
+	data: {
+    balance:0.00,
+    freeze:0,
+    score:0,
+    score_sign_continuous:0,
+    rechargeOpen: false // 是否开启充值[预存]功能
+  },
+	onLoad() {
+    let rechargeOpen = wx.getStorageSync('RECHARGE_OPEN')
+    if (rechargeOpen && rechargeOpen == "1") {
+      rechargeOpen = true
+    } else {
+      rechargeOpen = false
+    }
+    this.setData({
+      rechargeOpen: rechargeOpen
+    })
+	},	
+  onShow() {
+    let that = this;
+    let userInfo = wx.getStorageSync('userInfo')
+    if (!userInfo) {
+      app.goLoginPageTimeOut()
+    } else {
+      that.setData({
+        userInfo: userInfo,
+        version: CONFIG.version,
+        vipLevel: app.globalData.vipLevel
+      })
+    }
+    this.getUserApiInfo();
+    this.getUserAmount();
+  },
+  aboutUs : function () {
+    wx.showModal({
+      title: '关于我们',
+      content: '本系统基于开源小程序商城系统 https://github.com/EastWorld/wechat-app-mall 搭建，祝大家使用愉快！',
+      showCancel:false
+    })
+  },
+  getUserApiInfo: function () {
+    var that = this;
+    WXAPI.userDetail(wx.getStorageSync('token')).then(function (res) {
+      if (res.code == 0) {
+        let _data = {}
+        _data.apiUserInfoMap = res.data
+        console.log(res.data)
+        res.data.base.mobile = "13408572357"
+        if (res.data.base.mobile) {
+          _data.userMobile = res.data.base.mobile
+        }
+        that.setData(_data);
+      }
+    })
+  },
+  getUserAmount: function () {
+    var that = this;
+    WXAPI.userAmount(wx.getStorageSync('token')).then(function (res) {
+      if (res.code == 0) {
+        that.setData({
+          balance: res.data.balance.toFixed(2),
+          freeze: res.data.freeze.toFixed(2),
+          score: res.data.score
+        });
+      }
+    })
+  },
+  relogin:function(){
+    app.navigateToLogin = false;
+    app.goLoginPageTimeOut()
+  },
+  goAsset: function () {
+    wx.navigateTo({
+      url: "/pages/asset/index"
+    })
+  },
+  goScore: function () {
+    wx.navigateTo({
+      url: "/pages/score/index"
+    })
+  },
+  goOrder: function (e) {
+    wx.navigateTo({
+      url: "/pages/order-list/index?type=" + e.currentTarget.dataset.type
+    })
+  }
+})
