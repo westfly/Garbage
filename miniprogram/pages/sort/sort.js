9a7fa7a3fabf05c580686ba3a7e3df2e563d1e98
Rayan <yweiyun@gmail.com>
2019-08-04 20:04:33 +0800

    rework indexClick

--- miniprogram/pages/sort/sort.js
+++ miniprogram/pages/sort/sort.js
@@ -16,21 +16,7 @@ Page({
   onClick:function(e){
     console.log(JSON.stringify(e))
     var index = e.currentTarget.dataset.index
-    var indexClick=0;
-    switch (index){
-      case 0:
-      indexClick=1
-      break;
-       case 1:
-        indexClick = 2
-      break;
-      case 2:
-        indexClick = 3
-        break;
-      case 3:
-        indexClick = 4
-        break;
-    }
+    var indexClick= index + 1;
     wx.navigateTo({
       url: '/pages/ai/filter/filter?type=' + indexClick,
     })
